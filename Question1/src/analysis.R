f8020011902595e8e645e5c35ced3c3f39ea6866
Sangeetha Ramtenki <111932301+Sangeetha-Ramtenki@users.noreply.github.com>
2023-02-27 23:29:46 -0600

    Update analysis.R

--- Question1/src/analysis.R
+++ Question1/src/analysis.R
@@ -1,4 +1,11 @@
+###Loading the clean data
 clean_people_data <- read.csv("~/Desktop/SANGITA/MY ASSIGNMENTS/PDS/Ass1/question1/clean_data/clean_people_data.csv")
+
+###performing a t test against age and frailty
 t_test_result <- with(clean_people_data, t.test(Age~Frailty))
 t_test_result
+
+###saving the output to a file
 capture.output(t_test_result, file="~/Desktop/SANGITA/MY ASSIGNMENTS/PDS/Ass1/question1/results/test_results.txt")
+
+
