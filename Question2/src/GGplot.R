84d2fd0be29a2f0365eab403e61cbe4d6f914f6b
Sangeetha Ramtenki <111932301+Sangeetha-Ramtenki@users.noreply.github.com>
2023-02-27 21:16:35 -0600

    Update GGplot.R

--- Question2/src/GGplot.R
+++ Question2/src/GGplot.R
@@ -1,2 +1,3 @@
+StudentsPerformance <- read.csv('~/Desktop/SANGITA/MY ASSIGNMENTS/PDS/Ass1/question2/clean_data/StudentsPerformance.csv')
 df<-data.frame(StudentsPerformance$race.ethnicity,StudentsPerformance$test.preparation.course)
 ggplot(df,aes(StudentsPerformance$test.preparation.course))+ geom_bar(aes(fill = StudentsPerformance$race.ethnicity), position = "dodge")
