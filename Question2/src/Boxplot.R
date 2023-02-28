a7289b6d8850f6a26df0e9a04806187fc423b97e
Sangeetha Ramtenki <111932301+Sangeetha-Ramtenki@users.noreply.github.com>
2023-02-27 23:37:12 -0600

    Update Boxplot.R

--- Question2/src/Boxplot.R
+++ Question2/src/Boxplot.R
@@ -1,2 +1,2 @@
-StudentsPerformance <- read.csv('~/Desktop/SANGITA/MY ASSIGNMENTS/PDS/Ass1/question2/clean_data/StudentsPerformance.csv')
+StudentsPerformance <- read.csv('~/Desktop/SANGITA/MY ASSIGNMENTS/PDS/Ass1/question2/clean_data/cleaned_data.csv')
 boxplot(StudentsPerformance$math.score,StudentsPerformance$reading.score,StudentsPerformance$writing.score, main="Box plot for the scores", col=c("red","pink","darkgreen"))
