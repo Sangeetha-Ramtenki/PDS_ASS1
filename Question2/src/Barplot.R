4795c9be6359fb23803faa97cc112082c40797ea
Sangeetha Ramtenki <111932301+Sangeetha-Ramtenki@users.noreply.github.com>
2023-02-27 20:24:16 -0600

    Update Barplot.R

--- Question2/src/Barplot.R
+++ Question2/src/Barplot.R
@@ -1,3 +1,2 @@
 x <- sample(c(StudentsPerformance$race.ethnicity), 100, replace=TRUE)
-
-barplot(table(x),col=c("pink","red","blue","darkgreen","gold"),ylim=c(0,30))
+barplot(table(x),col=c("pink","red","blue","darkgreen","gold"),ylim=c(0,30),main="Barplot of race/ethinicty")
