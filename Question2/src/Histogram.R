07853b09dc4bf7831a54318fe3d92fa5b26166b9
Sangeetha Ramtenki <111932301+Sangeetha-Ramtenki@users.noreply.github.com>
2023-02-27 23:37:57 -0600

    Update Histogram.R

--- Question2/src/Histogram.R
+++ Question2/src/Histogram.R
@@ -1,4 +1,4 @@
-StudentsPerformance <- read.csv('~/Desktop/SANGITA/MY ASSIGNMENTS/PDS/Ass1/question2/clean_data/StudentsPerformance.csv')
+StudentsPerformance <- read.csv('~/Desktop/SANGITA/MY ASSIGNMENTS/PDS/Ass1/question2/clean_data/cleaned_data.csv'')
 
 totalscore <- StudentsPerformance$math.score + StudentsPerformance$reading.score + StudentsPerformance$writing.score
 
