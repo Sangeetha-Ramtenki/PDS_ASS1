f8458b635d7f4bcf696397d7db9887495f1e12e5
Sangeetha Ramtenki <111932301+Sangeetha-Ramtenki@users.noreply.github.com>
2023-02-27 21:16:12 -0600

    Update Barplot.R

--- Question2/src/Barplot.R
+++ Question2/src/Barplot.R
@@ -1,2 +1,3 @@
+StudentsPerformance <- read.csv('~/Desktop/SANGITA/MY ASSIGNMENTS/PDS/Ass1/question2/clean_data/StudentsPerformance.csv')
 x <- sample(c(StudentsPerformance$race.ethnicity), 100, replace=TRUE)
 barplot(table(x),col=c("pink","red","blue","darkgreen","gold"),ylim=c(0,30),main="Barplot of race/ethinicty")
