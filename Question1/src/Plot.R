9b4b8bb87d15e989d49cc556c48788d895c7ac28
Sangeetha Ramtenki <111932301+Sangeetha-Ramtenki@users.noreply.github.com>
2023-02-27 23:30:17 -0600

    Update Plot.R

--- Question1/src/Plot.R
+++ Question1/src/Plot.R
@@ -1,2 +1,5 @@
- People_data <- read.csv("~/Desktop/SANGITA/MY ASSIGNMENTS/PDS/Ass1/question1/clean_data/clean_people_data.csv")
- plot(People_data$Age, People_data$Weight.Pounds.,xlab="Age",ylab="Weight",main="Age vs Weight", col="red")
+###loading the data 
+People_data <- read.csv("~/Desktop/SANGITA/MY ASSIGNMENTS/PDS/Ass1/question1/clean_data/clean_people_data.csv")
+
+###visualizing the data
+plot(People_data$Age, People_data$Weight.Pounds.,xlab="Age",ylab="Weight",main="Age vs Weight", col="red")
