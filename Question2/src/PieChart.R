91d8d271adf64af25a1ef8fe02233ffde92f0cfc
Sangeetha Ramtenki <111932301+Sangeetha-Ramtenki@users.noreply.github.com>
2023-02-27 20:42:21 -0600

    Update PieChart.R

--- Question2/src/PieChart.R
+++ Question2/src/PieChart.R
@@ -2,4 +2,4 @@ table(StudentsPerformance$gender)
 n <- nrow(StudentsPerformance)
 percent_gender <- table(StudentsPerformance$gender)/n * 100
 percent_gender
-pie(percent_gender, main = "Gender PieChart", col = c("magenta","blue"))
+pie(percent_gender, main = "Gender PieChart", col = c("magenta","blue"),label=paste(c("female","male"),percent_gender, "%"))
