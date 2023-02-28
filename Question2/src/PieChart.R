be0de0e323786c611ce4b445c1e35f732ce77eb0
Sangeetha Ramtenki <111932301+Sangeetha-Ramtenki@users.noreply.github.com>
2023-02-27 20:38:49 -0600

    Create PieChart.R

---
+++ Question2/src/PieChart.R
@@ -0,0 +1,5 @@
+table(StudentsPerformance$gender)
+n <- nrow(StudentsPerformance)
+percent_gender <- table(StudentsPerformance$gender)/n * 100
+percent_gender
+pie(percent_gender, main = "Gender PieChart", col = c("magenta","blue"))
